01ae0b953b37920fd2ea642acf718c23719a76a5
luoxinnan <luoxinnan1@gmail.com>
2024-04-09 16:10:31 +0200

    Implement functin to add several ingreds for a dish

--- Frontend/src/components/AddDishDetailForm.tsx
+++ Frontend/src/components/AddDishDetailForm.tsx
@@ -1,8 +1,28 @@
-import { Props } from "./types/addDishDetailFormTypes";
-import Counter from "./Counter"
+
 import { BrowserRouter as Router, Route, Switch, Link, useHistory } from 'react-router-dom';
+import { useState } from "react";
+import { Ingred } from "@/App";
 
 export default function AddDishDetailForm() {
+    const [ingredForms, setIngredForms] = useState<Ingred[]>([])
+
+    function addIngredForm(){
+        const newIngredForms = [...ingredForms, {name: "", quantity: 0}]
+        setIngredForms(newIngredForms);
+    }
+
+    function addNameToIngred(name:string, i:number){
+        const newIngredForms = [...ingredForms];
+        newIngredForms[i].name = name;
+        setIngredForms(newIngredForms);
+    }
+
+    function addQuantityToIngred(quantity: string, i:number){
+        const newIngredForms = [...ingredForms];
+        newIngredForms[i].quantity = Number(quantity);
+        setIngredForms(newIngredForms);
+    }
+
     return (
         <div>
             <form className="max-w-sm mx-auto">
@@ -12,21 +32,41 @@ export default function AddDishDetailForm() {
                 </div>
                 <label>Ingredients: </label>
                 <section className="ingred-form">
-                    <div className="mb-5 ingred-form__name">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name: </label>
-                        <input type="text" name="dish-ingredient-name" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                    </div>
-                    <div className="mb-5 ingred-form__quantity">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantity: </label>
-                        <input type="text" name="dish-ingredient-quantity" className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                    </div>
-                    <button className="ingred-form__add-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">+</button>
+                    {
+                        ingredForms.map((ingred, i) =>
+                            <>
+                                <div className="mb-5 ingred-form__name">
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name: </label>
+                                    <input type="text" name="dish-ingredient-name" onChange={event => addNameToIngred(event.target.value, i)}className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                </div>
+                                <div className="mb-5 ingred-form__quantity">
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantity: </label>
+                                    <input type="text" name="dish-ingredient-quantity" onChange={event => addQuantityToIngred(event.target.value, i)}className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                </div>
+                                
+                                
+                            </>
+
+                        )
+
+                        
+                    }
+                    <button onClick= {addIngredForm} type="button" className="ingred-form__add-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">+</button>
                 </section>
             </form>
+
+            <p>----------for testing----------</p>
+            {
+                ingredForms.map((ingred) => <p>{ingred.name}, {ingred.quantity}</p>)
+            }
+            <p>-------------------------------</p>
+
+
+
             <br />
 
             <Link to="/dishes"><button>Cancel</button></Link>
-            <button >Confirm</button>
+            <button type="submit">Confirm</button>
             <br />
 
 
